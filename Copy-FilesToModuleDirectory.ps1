e47a94bf76621e292268764e6a93f18bb2284c7a
Nate Scherer <376408+natescherer@users.noreply.github.com>
2021-04-02 13:04:16 -0400

    Add handling for module publishing directory

---
+++ Copy-FilesToModuleDirectory.ps1
@@ -0,0 +1,25 @@
+function Copy-FilesToModuleDirectory {
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory = $true)]
+        [string]$Path,
+        [parameter(Mandatory = $true)]
+        [string]$ActionPath
+    )
+    # PowerShellGet currently requires the module be in a folder named the same as the manifest
+
+    $OutputPath = ""
+
+    Write-Host "Creating directory for module publishing..."
+
+    $ManifestName = Split-Path -Path $Path -LeafBase
+    $ManifestDir = Split-Path -Path $Path -Parent
+
+    $OutputPath = "$ActionPath\$ManifestName"
+
+    Write-Host "Copying files from $ManifestDir to $OutputPath..."
+    New-Item -Path $OutputPath -Type Directory | Out-Null
+    Copy-Item -Path "$ManifestDir\*" -Destination $OutputPath
+
+    $OutputPath
+}
