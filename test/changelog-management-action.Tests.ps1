9a790ac05b0582449a5530da98beaca4a4f24907
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-10-23 00:16:30 -0400

    Add first test and artifactory repo cleanup

--- test/changelog-management-action.Tests.ps1
+++ test/changelog-management-action.Tests.ps1
@@ -1,7 +1,11 @@
 $global:NL = [System.Environment]::NewLine
 
-Describe 'publish to packages' {
-    It 'check package exists' {
-        Get-Content -Path "test\read_lastversion.txt" -Raw | Should -Be "1.1.0"
+Describe 'Publish to NuGet' {
+    It 'Install Module' {
+        $ModuleName = "testmodule-$env:RUNNER_OS_LOWER"
+        $RepoName = "JFrog"
+        Register-PSRepository -Name $RepoName -SourceLocation "https://natescherer.jfrog.io/artifactory/api/nuget/changelog-management-action-$env:RUNNER_OS_LOWER" -InstallationPolicy Trusted
+        Install-PSResource -Name $ModuleName -Repository $RepoName
+        Get-PSResource -Name $ModuleName | Should -Not -BeNullOrEmpty
     }
 }
