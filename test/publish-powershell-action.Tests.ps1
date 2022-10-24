2b8c6c41d67f399d2839318b1e56284861f52e16
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-10-24 18:34:32 -0400

    Simplify nuget repos

--- test/publish-powershell-action.Tests.ps1
+++ test/publish-powershell-action.Tests.ps1
@@ -5,7 +5,7 @@ Describe 'Publish to NuGet' {
         $ModuleName = "TestModule-$env:RUNNER_OS_LOWER"
         $RepoName = "JFrog"
         $Version = "1.0.1-alpha$env:VERSION_SUFFIX"
-        Register-PSResourceRepository -Name $RepoName -Uri "https://natescherer.jfrog.io/artifactory/api/nuget/publish-powershell-action-$env:RUNNER_OS_LOWER" -Trusted
+        Register-PSResourceRepository -Name $RepoName -Uri "https://natescherer.jfrog.io/artifactory/api/nuget/nuget" -Trusted
         Install-PSResource -Name $ModuleName -Repository $RepoName -Version $Version -Prerelease
         Get-PSResource -Name $ModuleName | Should -Not -BeNullOrEmpty
     }
