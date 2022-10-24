48ad22fe03ecb8348fc84bee39723b1fe4c9ccef
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-10-24 16:37:24 -0400

    Switch back to making individual repos

--- test/publish-powershell-action.Tests.ps1
+++ test/publish-powershell-action.Tests.ps1
@@ -4,7 +4,7 @@ Describe 'Publish to NuGet' {
     It 'Install Module' {
         $ModuleName = "TestModule-$env:RUNNER_OS_LOWER"
         $RepoName = "JFrog"
-        Register-PSResourceRepository -Name $RepoName -Uri "https://natescherer.jfrog.io/artifactory/api/nuget/nuget" -Trusted
+        Register-PSResourceRepository -Name $RepoName -Uri "https://natescherer.jfrog.io/artifactory/api/nuget/publish-powershell-action-$env:RUNNER_OS_LOWER" -Trusted
         Install-PSResource -Name $ModuleName -Repository $RepoName
         Get-PSResource -Name $ModuleName | Should -Not -BeNullOrEmpty
     }
