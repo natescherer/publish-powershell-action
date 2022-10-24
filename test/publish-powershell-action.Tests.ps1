59445f149fef023e7e8a3829ca4602f44a6ae4f9
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-10-24 19:40:04 -0400

    Add packages test

--- test/publish-powershell-action.Tests.ps1
+++ test/publish-powershell-action.Tests.ps1
@@ -1,6 +1,21 @@
 $global:NL = [System.Environment]::NewLine
 
-Describe 'Publish to NuGet' {
+Describe 'Packages' {
+    It 'Install Module' {
+        $ModuleName = "TestModule-$env:RUNNER_OS_LOWER"
+        $RepoName = "Packages"
+        $Version = "1.0.1-alpha$env:VERSION_SUFFIX"
+        Register-PSResourceRepository -Name $RepoName -Uri https://nuget.pkg.github.com/natescherer/index.json -Trusted
+        $Username = "natescherer"
+        $Password = $env:GITHUB_TOKEN
+        $Credential = New-Object System.Management.Automation.PSCredential ($Username, $(ConvertTo-SecureString $Password -AsPlainText -Force))
+        Install-PSResource -Name $ModuleName -Repository $RepoName -Prerelease -Credential $Credential
+        Get-PSResource -Name $ModuleName | Should -Not -BeNullOrEmpty
+        Uninstall-PSResource -Name $ModuleName -Version $Version -Prerelease
+    }
+}
+
+Describe 'NuGet' {
     It 'Install Module' {
         $ModuleName = "TestModule-$env:RUNNER_OS_LOWER"
         $RepoName = "JFrog"
