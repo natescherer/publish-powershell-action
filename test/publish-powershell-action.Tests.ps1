e3fd5bcb78120d14afd599a49731b2256b5feff0
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-10-24 17:14:55 -0400

    Version modules

--- test/publish-powershell-action.Tests.ps1
+++ test/publish-powershell-action.Tests.ps1
@@ -4,8 +4,9 @@ Describe 'Publish to NuGet' {
     It 'Install Module' {
         $ModuleName = "TestModule-$env:RUNNER_OS_LOWER"
         $RepoName = "JFrog"
+        $Version = "1.0.1-alpha$env:VERSION_SUFFIX"
         Register-PSResourceRepository -Name $RepoName -Uri "https://natescherer.jfrog.io/artifactory/api/nuget/publish-powershell-action-$env:RUNNER_OS_LOWER" -Trusted
-        Install-PSResource -Name $ModuleName -Repository $RepoName
+        Install-PSResource -Name $ModuleName -Repository $RepoName -Version $Version -Prerelease
         Get-PSResource -Name $ModuleName | Should -Not -BeNullOrEmpty
     }
 }
