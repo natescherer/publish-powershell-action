985187104ad4e4d649d31547a5f84e7a8898ef59
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-11-04 02:49:37 -0400

    Fix gallery publishing

--- src/steps/5_publish_to_gallery.ps1
+++ src/steps/5_publish_to_gallery.ps1
@@ -1,6 +1,3 @@
-Write-Host "Registering PowerShell Gallery..."
-Register-PSResourceRepository -PSGallery -Trusted
-
 Write-Host "Publishing to PowerShell Gallery..."
 Publish-PSResource -Path $env:RESOLVED_PATH -Repository "PSGallery" -ApiKey $env:INPUT_TOKEN
 
