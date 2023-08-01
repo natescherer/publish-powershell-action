08469d53ac8ba7ea8ec8126cab2b0b4064f29986
Nate Scherer <376408+natescherer@users.noreply.github.com>
2023-08-01 12:28:19 -0400

    Pin action to to PSResourceGet 0.5.23-beta23

--- src/steps/2_prepare.ps1
+++ src/steps/2_prepare.ps1
@@ -1,11 +1,38 @@
 Write-Host "Starting Action..."
 
-Write-Host "Installing PowerShellGet 3..."
-# Ensuring PowerShellGet stable is latest version
-Install-Module -Name PowerShellGet -Force -AllowClobber
-# Installing PowerShellGet 3 Prerelease
-# Pinned to old version due to https://github.com/PowerShell/PowerShellGet/issues/835
-Install-Module -Name PowerShellGet -RequiredVersion 3.0.16-beta16 -AllowPrerelease -Force -Repository PSGallery -SkipPublisherCheck
+$PsrgPinnedVerShort = "0.5.23"
+$PsrgPinnedVer = "0.5.23-beta23"
+
+Write-Host -Object "Ensuring PowerShellGet 2.2.5 is installed..."
+# Ensuring PowerShellGet stable is at least version 2.2.5
+$PowerShellGetMetadata = Get-Module -Name PowerShellGet
+if ($PowerShellGetMetadata.Version -ge "2.2.5") {
+    Write-Host -Object "Updating PowerShellGet to 2.2.5..."
+    Install-Module -Name PowerShellGet -Force -AllowClobber -Scope AllUsers
+}
+else {
+    Write-Host -Object "PowerShellGet is up-to-date."
+}
+
+# Ensuring Microsoft.PowerShell.PSResourceGet is installed
+$PSResourceGetMetadata = Get-Module -ListAvailable -Name Microsoft.PowerShell.PSResourceGet
+if (!$PSResourceGetMetadata) {
+    Write-Host -Object "'Microsoft.PowerShell.PSResourceGet' is not installed, now installing..."
+    Install-Module -Name Microsoft.PowerShell.PSResourceGet -RequiredVersion $PsrgPinnedVer -Force -AllowPrerelease -Scope AllUsers
+}
+elseif ($PSResourceGetMetadata.Version -gt $PsrgPinnedVerShort) {
+    Write-Warning -Message "'Microsoft.PowerShell.PSResourceGet' is higher than the pinned version of '$PsrgPinnedVer'. This may cause unexpected results. Consider opening a GitHub issue at 'https://github.com/natescherer/devcontainers-custom-features/issues' regarding this."
+}
+elseif ($PSResourceGetMetadata.Version -lt $PsrgPinnedVerShort) {
+    Write-Host "'Microsoft.PowerShell.PSResourceGet' is less than the pinned version of '$PsrgPinnedVer'. Now updating..."
+    Install-Module -Name Microsoft.PowerShell.PSResourceGet -RequiredVersion $PsrgPinnedVer -Force -AllowPrerelease -Scope AllUsers
+}
+elseif ($PSResourceGetMetadata.Version -eq $PsrgPinnedVerShort) {
+    Write-Host "'Microsoft.PowerShell.PSResourceGet' is already at pinned version of '$PsrgPinnedVer'."
+}
+else {
+    throw "Something went wrong while ensuring 'Microsoft.PowerShell.PSResourceGet' is installed. Consider opening a GitHub issue at 'https://github.com/natescherer/devcontainers-custom-features/issues' regarding this."
+}
 
 $FullPath = "$env:GITHUB_WORKSPACE\$env:INPUT_PATH"
 
